5ab194594d694100dff3c0a43e068f6dad95f550
Kitty <kitty.giraudel@gmail.com>
2023-10-03 09:29:36 +0200

    Update an import path

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Footnotes, FootnotesProvider, FootnoteRef, getIdFromTree } from './'
 import { render, configure, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom'
 
 configure({ testIdAttribute: 'id' })
 
